5fab86fe10d3b6904e5d5aa119e469e2ef5d2476
Jemuel <f2pskilleruel@gmail.com>
2022-04-11 18:31:36 +0300

    Add placemark function

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 // script
 
-const gameSquares = []
+
 
 const gameBoard = (() => {
 
+    const gameSquares = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+
     const createBoard = () => {
         const board = document.getElementById("board")
-        for (i = 0; i < 9; i++) {
+        gameSquares.forEach((element) => {
             var cell = document.createElement("div")
             cell.classList.add("cell")
-            cell.setAttribute("data-cell", i+1)
+            cell.setAttribute("data-cell", element)
             board.appendChild(cell)
-            cell.addEventListener("click", () => {
-                console.log("I work!")
-            })
-        }
+            cell.addEventListener("click", displayController.getClick, {once : true})
+        })
     }
 
     return {
-        createBoard
+        createBoard,
+        gameSquares
+    }
+})()
+
+const displayController = (() => {
+
+    let circleTurn
+    const CIRCLE_CLASS = "o"
+    const X_CLASS = "x"
+
+    const getClick = function handleClick(e) {
+        const cellTarget = e.target
+        const cellNumber = e.target.dataset.cell
+        const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+        placemark(cellTarget, currentClass, cellNumber)
+        circleTurn = !circleTurn
+    }
+
+    function placemark(cell, currentClass, index) {
+        cell.classList.add(currentClass)
+        gameBoard.gameSquares[index] = currentClass
+        console.log(gameBoard.gameSquares)
+    }
+
+    return {
+        getClick
     }
 })()
 
